cbb52e04afeba6f9b1766b6e56074548821e7a30
SaToGo1 <85353835+SaToGo1@users.noreply.github.com>
2023-06-09 00:12:00 +0000

    Database Connection established

--- index.js
+++ index.js
@@ -3,16 +3,25 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
-// const mongoose = require('mongoose')
+const URI = process.env['MY_URI']
+const mongoose = require('mongoose')
+
 // const bodyParser = require('body-parser')
+
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(data => {
+    console.log('\n', 'connected to the Database')
+  })
+  .catch(err => console.log('\n', err))
 
 // const userSchema = mongoose.Schema({
 //   username: String,
 // })
 
+
 app.use(cors())
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded())
+// app.use(bodyParser.urlencoded())
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
